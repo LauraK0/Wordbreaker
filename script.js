27105c7a6790bb6c17acb72c4620914b5be01101
LauraK0 <laurakeating@live.co.uk>
2022-11-09 21:22:24 +0000

    boilerplate functions added and restart button removed on click
    relates #10

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const startButton = document.getElementById("start-button")
 startButton.addEventListener("click", (e) => {
     e.preventDefault(); 
-    getWord(); 
+    getWord();
+    startGame();
 })
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); // stop the form's default behaviour of submitting
+    const input = document.querySelector("input");
+    const guessedWord = 'input.value.toLowerCase().trim()'; 
+    getDefinition(guessedWord);
+})
+
+function startGame() {
+  if (document.body.contains(document.getElementById("start-button"))){  
+    document.getElementById("start-button").remove();
+}
+}
 
 function getWord(){
     fetch(`https://random-word-api.herokuapp.com/word?length=5`)
@@ -22,4 +37,26 @@ function getWord(){
           const list = document.querySelector(".search-results-description");
           list.appendChild(para);
       }) 
+    }
+
+function checkIfDictionaryWord(word) {
+  // request that word from dictionary API
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then((response) => {
+        if (!response.ok) throw new Error(response.status); 
+        return response.json();
+    })
+    .catch(error => {
+        console.log(error);
+        let para = document.createElement("p");
+        let str = "Please enter a di";
+        const definition = document.createTextNode(str);
+        para.appendChild(definition);
+        const list = document.querySelector(".search-results-description");
+        list.appendChild(para);
+    }) 
+  }
+
+    function compareWord() {
+
     }
