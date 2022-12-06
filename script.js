3965de545158441112a5711ba176d1e2f78ca944
Laura Keating <108976875+LauraK0@users.noreply.github.com>
2022-12-06 16:18:07 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,76 @@
 const startButton = document.getElementById("start-button");
-const gameContainer = document.getElementById('game-container');
 
-let height = 6; //number of guesses
-let width = 5; //length of the word
-
+let modal;
+let heightString;
+let widthString;
+let height;
+let width;
 let row = 0; //current guess (attempt #)
 let col = 0; //current letter for that attempt
-
-let word = '';
+let winningWord;
 let gameOver = false;
+let closeButton;
+let resetButton;
+
+// document.addEventListener("DOMContentLoaded", );
+
+document.addEventListener("keyup", (e) => {
+    processInput(e);
+})
 
 startButton.addEventListener("click", (e) => {
     e.preventDefault(); 
-    getWord();
+    heightString = document.getElementById('number-of-guesses').value; //number of guesses
+    widthString = document.getElementById('length-of-word').value; //length of the word
+    height = Number(heightString); //number of guesses
+    width = Number(widthString); //length of the word
     removeStartButton();
+    getWinningWord()
     generateGameBoard();
 })
 
 function removeStartButton() {
   if (document.body.contains(document.getElementById("start-button"))){  
     document.getElementById("start-button").remove();
+    document.getElementById('game-init').remove();
 }
 }
 
-function getWord(){
-  fetch(`https://random-word-api.herokuapp.com/word?length=5`)
+function getWinningWord(){
+    fetch(`https://random-word-api.herokuapp.com/word?length=${width}`)
     .then((response) => {
         if (!response.ok) throw new Error(response.status); 
         return response.json();
     })
     .then (response => {
-    word = response[0];
-    console.log(word);
+    winningWord = response[0].toUpperCase();
+    console.log(winningWord);
       })
     .catch(error => {
         console.log(error);
     }) 
   }
 
-// generates Game //
+  // generates Game //
 
 function generateGameBoard() {
   const gameBoard = document.createElement('div'); 
   gameBoard.className = 'game-area';
-
   const gameGrid = document.createElement('div'); 
   gameGrid.className = 'game-grid';
+  gameGrid.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
 
   const keyboardContainer = document.createElement('div'); 
   keyboardContainer.className = 'keyboard-container';
-
   for (let r = 0; r < height; r++) {
     for (let c = 0; c < width; c++) {
-        // <span id="0-0" class="tile">P</span>
-        let tile = document.createElement('span');
-        tile.id = r.toString() + "-" + c.toString();
-        tile.classList.add('row-block');
-        tile.innerText = '';
-        gameGrid.append(tile);
+    let tileHTML = 
+    `<span id='${r}-${c}' class='row-block'></span>`;
+    gameGrid.innerHTML += tileHTML; 
     }
   document.body.append(gameBoard);
   gameBoard.append(gameGrid);
 }
-
   let keyboard = [
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
     ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
@@ -89,7 +97,7 @@ for (let i = 0; i < keyboard.length; i++) {
         else if ("A" <= key && key <= "Z") {
             keyTile.id = "Key" + key; 
         } 
-
+        keyTile.classList.add("unused");
         keyTile.addEventListener("click", processKey);
 
         if (key == "Enter") {
@@ -109,18 +117,12 @@ for (let i = 0; i < keyboard.length; i++) {
     spacerTwo.classList.add("spacer-half");
     let middleRow = document.getElementById('A,S,D,F,G,H,J,K,L');
     let buttonA = document.getElementById('KeyA');
-    console.log(buttonA);
     middleRow.insertBefore(spacerOne, buttonA);
     middleRow.appendChild(spacerTwo);
-
-
-    // Listen for Key Press
-    document.addEventListener("keyup", (e) => {
-      processInput(e);
-  })
 }
 
 //end of generate game function//
+
 
 //functions to handle and compare words //
 
@@ -130,120 +132,207 @@ function processKey() {
 }
 
 function processInput(e) {
-  if (gameOver) return; 
+    if (gameOver) return; 
 
-  // alert(e.code);
-  if ("KeyA" <= e.code && e.code <= "KeyZ") {
-      if (col < width) {
-          let currTile = document.getElementById(row.toString() + '-' + col.toString());
-          if (currTile.innerText == "") {
-              currTile.innerText = e.code[3];
-              col += 1;
-          }
-      }
-  }
-  else if (e.code == "Backspace") {
-      if (0 < col && col <= width) {
-          col -=1;
-      }
-      let currTile = document.getElementById(row.toString() + '-' + col.toString());
-      currTile.innerText = "";
-  }
+    if ("KeyA" <= e.code && e.code <= "KeyZ") {
+        if (col < width) {
+            let currTile = document.getElementById(`${row}-${col}`);
+            if (currTile.innerText == "") {
+                currTile.innerText = e.code[3];
+                col += 1;
+            }
+        }
+    }
+    else if (e.code == "Backspace") {
+        if (0 < col && col <= width) {
+            col -=1;
+        }
+        let currTile = document.getElementById(`${row}-${col}`);
+        currTile.innerText = "";
+    }
+  
+    else if (e.code == "Enter") {
+      (col === width) ? update(): alert(`Please enter a ${width} letter word`);
+    }
+  
+    if (!gameOver && row == height) {
+      console.log('game is over');
+        gameOver = true;
+        losingModal();
+    }
 
-  else if (e.code == "Enter") {
-      update();
-  }
 
-  if (!gameOver && row == height) {
-      gameOver = true;
-      //document.getElementById("answer").innerText = word;
-  }
 }
 
 function update() {
   let guess = "";
-  //document.getElementById("answer").innerText = "";
 
   //string up the guesses into the word
   for (let c = 0; c < width; c++) {
-      let currTile = document.getElementById(row.toString() + '-' + c.toString());
+      let currTile = document.getElementById(`${row}-${c}`);
       let letter = currTile.innerText;
       guess += letter;
   }
-
-  guess = guess.toLowerCase(); //case sensitive
   console.log(guess);
+    guess = guess.toLocaleLowerCase(); //case sensitive
+    checkIfRealWord(guess);
+}
 
-  if (!guessList.includes(guess)) {
-      //document.getElementById("answer").innerText = "Not in word list";
-      return;
-  }
-  
-  //start processing guess
-  let correct = 0;
+function checkIfRealWord(guess){
+    if (winningWord === guess.toLocaleUpperCase()) {
+        gameWon();
+    }
+    else {
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${guess}`)
+            .then((response) => {
+                if (!response.ok) throw new Error(response.status); 
+                return response.json();
+            })
+            .then((data_arr) => { //returns object containing data relating to work
+                data_arr.every ( data => { //looping through each item in the object
+                        flipTiles(data.word);
+                        })
+                    })
+            .catch(error => {
+                console.log(error);
+                alert('Please enter a 5 letter word found in the dictionary');
+            }) 
+        }
+}
 
-  let letterCount = {}; //keep track of letter frequency, ex) KENNY -> {K:1, E:1, N:2, Y: 1}
-  for (let i = 0; i < word.length; i++) {
-      let letter = word[i];
 
-      if (letterCount[letter]) {
-         letterCount[letter] += 1;
-      } 
-      else {
-         letterCount[letter] = 1;
+function flipTiles() {
+    let currentGuess = []
+    for (let c = 0; c < width; c++) {
+        let currTile = document.getElementById(`${row}-${c}`);
+        let letter = currTile.innerText;
+        currentGuess.push({ tile: `${row}-${c}`, value: letter});
+    }
+
+    let winningArray = winningWord.split('');
+
+    currentGuess.forEach((letter, index) => {
+        if (letter.value == winningArray[index]) {
+            let currTile = document.getElementById(letter.tile);
+            let keyTile = document.getElementById("Key" + letter.value);
+            currTile.classList.add('correct');
+            keyTile.classList.remove("unused");
+            keyTile.classList.add('correct');
+        }
+    })
+
+    currentGuess.forEach(letter => {
+        if (winningArray.includes(letter.value)) {
+            let currTile = document.getElementById(letter.tile);
+            let keyTile = document.getElementById("Key" + letter.value);
+            if (currTile.classList.value !== 'row-block correct') {
+                currTile.classList.add('present');
+                keyTile.classList.remove("unused");
+                keyTile.classList.add('present');
+            }
+        }
+    })
+
+    currentGuess.forEach(letter => {
+        if (!winningArray.includes(letter.value)) {
+            let currTile = document.getElementById(letter.tile);
+            let keyTile = document.getElementById("Key" + letter.value);
+            if (currTile.classList.value !== 'row-block correct' || currTile.classList.value !== 'row-block present') {
+                currTile.classList.add('absent');
+                keyTile.classList.remove("unused");
+                keyTile.classList.add('absent');
+            }
+        }
+    })
+
+    row += 1; //start new row
+    col = 0; //start at 0 for new row
+
+    if (row >= height) {
+        losingModal();
       }
-  }
+}
 
-  console.log(letterCount);
+function gameWon() {
+    flipTiles();
+    winningModal();
+}
 
-  //first iteration, check all the correct ones first
-  for (let c = 0; c < width; c++) {
-      let currTile = document.getElementById(row.toString() + '-' + c.toString());
-      let letter = currTile.innerText;
-
-      //Is it in the correct position?
-      if (word[c] == letter) {
-          currTile.classList.add("correct");
-
-          let keyTile = document.getElementById("Key" + letter);
-          keyTile.classList.remove("present");
-          keyTile.classList.add("correct");
-
-          correct += 1;
-          letterCount[letter] -= 1; //deduct the letter count
-      }
-
-      if (correct == width) {
-          gameOver = true;
-      }
-  }
-
-  console.log(letterCount);
-  //go again and mark which ones are present but in wrong position
-  for (let c = 0; c < width; c++) {
-      let currTile = document.getElementById(row.toString() + '-' + c.toString());
-      let letter = currTile.innerText;
-
-      // skip the letter if it has been marked correct
-      if (!currTile.classList.contains("correct")) {
-          //Is it in the word?         //make sure we don't double count
-          if (word.includes(letter) && letterCount[letter] > 0) {
-              currTile.classList.add("present");
-              
-              let keyTile = document.getElementById("Key" + letter);
-              if (!keyTile.classList.contains("correct")) {
-                  keyTile.classList.add("present");
-              }
-              letterCount[letter] -= 1;
-          } // Not in the word or (was in word but letters all used up to avoid overcount)
-          else {
-              currTile.classList.add("absent");
-              let keyTile = document.getElementById("Key" + letter);
-              keyTile.classList.add("absent")
+function winningModal() {
+    modal = document.createElement('div'); 
+    modal.classList.add('modal');
+    const modalHTML = 
+    `
+        <div id="modal-content">
+        <span class="close-button">×</span>
+        <h2 class="search-results-heading">You've Won</h2>
+        <button class="standard-button" type="reset" id="reset-button">Play Again?</button>
+    `;
+    modal.innerHTML = modalHTML; 
+    document.body.appendChild(modal); // add new section to DOM for displaying search results
+    modal.classList.toggle("show-modal");
+    closeButton = document.querySelector(".close-button");
+    resetButton = document.getElementById('reset-button');
+    document.addEventListener(
+        "click",
+        function (event) {
+          // If user either clicks X button OR clicks outside the modal window, then close modal by calling closeModal()
+          if (
+            event.target.matches(".close-button") ||
+            !event.target.closest(".modal-content")
+          ) {
+            closeModal();
           }
-      }
-  }
+        },
+        false
+      );
+    // closeButton.addEventListener("click", closeModal);
+    resetButton.addEventListener("click", reset);
+}
 
-  row += 1; //start new row
-  col = 0; //start at 0 for new row
+function losingModal() {
+    modal = document.createElement('div'); 
+    modal.classList.add('modal');
+    const modalHTML = 
+    `
+        <div id="modal-content">
+        <h2 class="search-results-heading">You've Lost!</h2>
+        <p>The winning word was ${winningWord}!</p>
+        <button class="standard-button" type="reset" id="reset-button">Play Again?</button>
+    `;
+    modal.innerHTML = modalHTML; 
+    document.body.appendChild(modal); // add new section to DOM for displaying search results
+    modal.classList.toggle("show-modal");
+    resetButton = document.getElementById('reset-button');
+    document.addEventListener(
+        "click",
+        function (event) {
+          // If user either clicks X button OR clicks outside the modal window, then close modal by calling closeModal()
+          if (
+            !event.target.closest(".modal-content")
+          ) {
+            closeModal();
+          }
+        },
+        false
+      );
+    // closeButton.addEventListener("click", closeModal);
+    resetButton.addEventListener("click", reset);
+}
+
+
+function closeModal(){
+    modal.remove();
+}
+
+function reset() {
+    closeModal();
+    let game = document.querySelector('.game-area');
+    let keyboard = document.querySelector('.keyboard-container');
+    game.remove();
+    keyboard.remove();
+    generateGameBoard();
+    getWinningWord();
+    row = 0; //start new row
+    col = 0; //start at 0 for new row
 }
